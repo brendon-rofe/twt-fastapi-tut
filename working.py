fc59d12a6e3460ef072674ef835a66ae64cddd10
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-06-25 15:01:29 +0200

    feat: Implement get-by-name endpoint

--- working.py
+++ working.py
@@ -10,3 +10,10 @@ def home():
 @app.get("/item/{item_id}")
 def get_item(item_id: int):
   return inventory[item_id]
+
+@app.get("/get-by-name")
+def get_by_name(name: str = None):
+  for item_id in inventory:
+    if inventory[item_id]["name"] == name:
+      return inventory[item_id]
+  return {"Data": "Not found"}
